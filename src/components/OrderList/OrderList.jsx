6bf4c85922fbe03dcf588748d90917ffbb561d62
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-03-30 01:37:23 +0600

    shipping,tax and grandTotal done

--- src/components/OrderList/OrderList.jsx
+++ src/components/OrderList/OrderList.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 
 const OrderList = ({food}) => {
-    console.log(food)
+   // console.log(food)
 
     let totalPrice = 0;
-
+    let shipping = 0;
+    let grandTotal = 0;
     for(let obj of food){
 
         totalPrice = totalPrice + obj.price ;
+        shipping = shipping + obj.shipping ;
     }
+
+    let tax = ( totalPrice * 7 ) / 100 ; 
+    grandTotal = totalPrice + shipping + tax; 
 
     return (
         <div className='bg-red-600 text-white rounded-lg p-5 sticky top-0'>
             <h2 className='text-2xl text-center underline'>Order List</h2>
             <p className='text-xl mt-5'>Price : ${totalPrice} </p>
+            <p className='text-xl my-3'>Shipping : ${shipping} </p>
+            <p className='text-xl my-3'>Tax : ${tax.toFixed(2)} </p>
+            <p className='text-xl my-3 text-center'>Grand Total : ${grandTotal.toFixed(2)} </p>
 
         </div>
     );
